293378a1a9fc0fb7c795ae020775f4cfac29e4b9
razazain <razazain861@gmail.com>
2024-10-10 13:43:33 +0500

    creating server and create basic api

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const chats = require("./data/data");
+const dotenv = require("dotenv").config();
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.send("api is running");
+})
+
+
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+})
+
+
+
+app.get('/api/chat/:id', (req, res) => {
+    console.log(req.params.id)
+    const singleChat = chats.find(c => c._id === req.params.id);
+    res.send(singleChat);
+})
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT 
+app.listen(PORT,()=>{
+    console.log(`server is running on the port ${PORT}`);
+})
