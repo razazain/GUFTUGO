9bb234dd29767c90390f8a51ad3b2b6fd5fab685
unknown <razazain861@gmail.com>
2025-02-22 12:30:59 +0500

    single chat work is remaining

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -5,8 +5,9 @@ import axios from 'axios';
 import { AddIcon } from '@chakra-ui/icons';
 import ChatLoading from './ChatLoading';
 import { getSender } from '../config/ChatLogics';
+import GroupChatModal from './miscellaneous/GroupChatModal';
 
-const MyChats = () => {
+const MyChats = ({ fetchAgain }) => {
   const [loggedUser, setLoggedUser] = useState();
 
   const { selectedChat, setSelectedChat, user, chats, setChats } = ChatState();
@@ -40,7 +41,7 @@ const MyChats = () => {
     setLoggedUser(JSON.parse(localStorage.getItem("userInfo")));
     fetchChats();
     // eslint-disable-next-line
-  }, []);
+  }, [fetchAgain]);
 
 
 
@@ -67,7 +68,7 @@ const MyChats = () => {
           alignItems="center"
         >
           My Chats
-          {/* <GroupChatModal> */}
+          <GroupChatModal> 
             <Button
               display="flex"
               fontSize={{ base: "17px", md: "10px", lg: "17px" }}
@@ -75,7 +76,7 @@ const MyChats = () => {
             >
               New Group Chat
             </Button>
-          {/* </GroupChatModal>  */}
+          </GroupChatModal> 
         </Box>
         <Box
           display="flex"
---
+++ frontend/src/components/SingleChat.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleChat = ({ fetchAgain, setFetchAgain }) => {
+  return (
+    <div>SingleChat</div>
+  )
+}
+
+export default SingleChat
